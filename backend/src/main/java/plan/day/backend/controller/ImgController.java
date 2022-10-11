286f2fc7900788b7277119db7cdc5854611b7ec3
Lihao Ding <lihaoding@LihaodeMacBook-Air.local>
2022-10-11 16:22:38 +0800

    delete

--- backend/src/main/java/plan/day/backend/controller/ImgController.java
+++ backend/src/main/java/plan/day/backend/controller/ImgController.java
@@ -3,13 +3,6 @@ package plan.day.backend.controller;
 
 
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.Gson;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -26,15 +19,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import plan.day.backend.annotation.CurrentUser;
-import plan.day.backend.model.CustomUserDetails;
-import plan.day.backend.model.Income;
-import plan.day.backend.payload.request.AddIncomeRequest;
 import plan.day.backend.payload.request.ImageRecognizeRequest;
 import plan.day.backend.payload.response.GeneralApiResponse;
 
 import javax.validation.Valid;
-import java.io.Closeable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,13 +31,7 @@ import java.util.Map;
 @RequestMapping("/api/img")
 public class ImgController {
 
-
-    /*@PostMapping("/recImg")
-    public ResponseEntity<?> recogniseImg(
-
-    }*/
-
-    @PostMapping("/add")
+    @PostMapping("/recImg")
     public ResponseEntity<?> createIncome(
             @Valid @RequestBody ImageRecognizeRequest request) {
         String imgRecognizeResult = doPost(request.getUrl());
